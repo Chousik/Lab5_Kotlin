21eb20f22b12c0be08484b034f61cbda5d935d50
Zahar_S <Zaxap2018@yandex.ru>
2024-02-28 21:31:48 +0300

    Remove .gradle from version control

--- src/main/kotlin/commands/ACommand.kt
+++ src/main/kotlin/commands/ACommand.kt
@@ -5,7 +5,6 @@ import org.chousik.exception.ArgumentCountError
 
 abstract class ACommand(private val name: String, private val info: String, private val countsArgument: Int) {
 
-    @Throws(ArgumentCountError::class)
     fun validCountsArgument(args: Array<String>) {
         if (args.size != countsArgument) {
             throw ArgumentCountError(countsArgument, args.size)
