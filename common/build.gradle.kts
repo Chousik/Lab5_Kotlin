29c7ee67c394eab63235f0e3c5139e1a9220ea14
Zahar_S <Zaxap2018@yandex.ru>
2024-05-08 13:13:04 +0300

    repair log4

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -14,8 +14,8 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
-    implementation("org.apache.poi:poi:5.2.0")
-    implementation("org.apache.poi:poi-ooxml:5.2.0")
+    implementation("org.apache.logging.log4j:log4j-api:2.23.1")
+    implementation("org.apache.logging.log4j:log4j-core:2.23.1")
     implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
